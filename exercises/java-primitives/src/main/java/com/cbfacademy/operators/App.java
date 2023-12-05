5cce4f211e9c74db1ace74362c480c2a10d34b44
Gaby <gaby@GabMac.local>
2023-12-05 17:43:09 +0000

    rebase

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -6,6 +6,7 @@ public class App {
     }
 
     public static double add(double operand1, double operand2) {
+<<<<<<< c76b9a576c7c0785e4a594978d5b074cc896180b
         return operand1 + operand2;
     }
 
@@ -31,5 +32,36 @@ public class App {
     public static Boolean isMoreThan(double operand1, double operand2) {
             return operand1 > operand2;
 
+=======
+        double result= operand1+operand2;
+        return result;        
+        
+    }
+
+    public static double subtract(double operand1, double operand2) {
+        double result = operand1-operand2;
+        return result;
+    }
+
+    public static double multiply(double operand1, double operand2) {
+        double result = operand1*operand2;
+        return result;
+        
+    }
+
+    public static Boolean areEqual(double operand1, double operand2) {
+        boolean result = operand1==operand2;
+        return result;
+    }
+
+    public static Boolean isLessThan(double operand1, double operand2) {
+        boolean result = operand1 < operand2;
+        return result;
+    }
+
+    public static Boolean isMoreThan(double operand1, double operand2) {
+        boolean result =  operand1 > operand2;
+        return result;
+>>>>>>> saving changes that were made before main was up to date
     }
 }
--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -9,12 +9,18 @@ public class App {
 
     public static String concatenate(String word1, String word2, String word3) {
         // TODO: Write code that concatenates the input parameters and returns the result
+<<<<<<< c76b9a576c7c0785e4a594978d5b074cc896180b
         return  word1.concat(word2).concat(word3); 
         
+=======
+           String result= word1+word2+word3;
+           return result;
+>>>>>>> saving changes that were made before main was up to date
     }
 
     public static Boolean areEqual(String word1, String word2) {
         // TODO: Write code to determine whether the input parameters are equal strings
+<<<<<<< c76b9a576c7c0785e4a594978d5b074cc896180b
         if (word1.equals(word2)){
             return true;
         }else{
@@ -25,5 +31,17 @@ public class App {
     public static String format(String item, int quantity, double price) {
         // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]"
         return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
+=======
+        Boolean result= word1==word2;
+        return result;
+
+    }
+
+    public static String format(String item, int quantity, double price) {
+        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
+        String result = "Item: " + item + ". Price: £" + price + ". Quantity: " + quantity;
+        return result;
+>>>>>>> saving changes that were made before main was up to date
     }
 }
+
