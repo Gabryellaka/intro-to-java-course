f09403585890ee255e7989dadb1a2503b2b28d71
Gaby <gaby@GabMac.local>
2023-12-05 17:48:17 +0000

    sorting conflicts

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -6,33 +6,6 @@ public class App {
     }
 
     public static double add(double operand1, double operand2) {
-<<<<<<< c76b9a576c7c0785e4a594978d5b074cc896180b
-        return operand1 + operand2;
-    }
-
-    public static double subtract(double operand1, double operand2) {
-            return operand1 - operand2;
-
-    }
-
-    public static double multiply(double operand1, double operand2) {
-            return operand1 * operand2;
-
-    }
-
-    public static Boolean areEqual(double operand1, double operand2) {
-            return operand1 == operand2;
-    }
-
-    public static Boolean isLessThan(double operand1, double operand2) {
-            return operand1 < operand2;
-
-    }
-
-    public static Boolean isMoreThan(double operand1, double operand2) {
-            return operand1 > operand2;
-
-=======
         double result= operand1+operand2;
         return result;        
         
@@ -62,6 +35,5 @@ public class App {
     public static Boolean isMoreThan(double operand1, double operand2) {
         boolean result =  operand1 > operand2;
         return result;
->>>>>>> saving changes that were made before main was up to date
     }
 }
